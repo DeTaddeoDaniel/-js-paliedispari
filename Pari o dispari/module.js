d6a6885af4be7b314f3097e6da375fa67ceef5f7
Daniel2711 <detaddeo.d@gmail.com>
2020-12-04 13:02:08 +0100

    Indovina pari e dispari completato

--- Pari o dispari/module.js
+++ Pari o dispari/module.js
@@ -31,18 +31,78 @@ function numeroInserito(){
     
 }
 
+/* numero casuale */
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //Il max è escluso e il min è incluso
+}
 /* Programma*/
 function app(){
 
-    /*inserisci numero*/
-    var numero = numeroInserito();
+    /* input corretto */
+    varinputCorretto = false;
+    var numero = -1;
+
+    do{
+        /*inserisci numero*/
+        numero = numeroInserito();
+
+        /* input check valori*/
+        if (numero > 0 && numero < 6){
+            console.log("Numero inserito valido: " + numero);
+            varinputCorretto = true;
+        } else {
+            console.log("numero inserito non valido o non complesso tra 1 e 5");
+            varinputCorretto = false;
+        }
+
+    } while (!varinputCorretto);
+
+    /* genra numero casuale*/
+    var numeroComputer = getRandomInt(1,5);
+    console.log("Numero computer: "+ numeroComputer);
+
+    /* Somma numeri*/
+    var somma = numero + numeroComputer;
+    console.log("Somma numero: " + somma);
     
+    /* Domanda finale*/
+    var risposta = prompt("Il numero finale è pari o dispari, inserisci: \np per pari \nd per dispari");
+    var checkRisposta = false;
+    var soluzione = "";
+
+    do{
+
+        risposta = prompt("Il numero finale è pari o dispari, inserisci: \np per pari \nd per dispari");
+        
+        if(risposta.toLowerCase() == "p" || risposta == "d"){
+            console.log("Risposta valida");
+            checkRisposta = true;
+        } else {
+            console.log("Risposta non valida");
+            checkRisposta = false;
+        }
+
+    } while( !checkRisposta);
+   
+
     /* Tipologia di numero */
-    if( numero % 2 == 0){
+    if( somma % 2 == 0){
         console.log(" Il numero è pari");
+        soluzione = "p";
     } else {
         console.log("Il numero è dispari");
+        soluzione = "d";
     }
+
+    /* Risposta finale*/
+    if (soluzione == risposta){
+        console.log("Complimenti, hai indovinato");
+    } else {
+        console.log("Mi spiace, non hai indovinato");
+    }
+    
 
 }
 
